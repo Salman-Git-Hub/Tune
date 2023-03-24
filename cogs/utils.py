04ab02e88638b30f5f6fedae694f86b9d95f5c63
Salman Ali <sa1713791@gmail.com>
2023-03-24 17:12:35 +0545

    update utils

--- cogs/utils.py
+++ cogs/utils.py
@@ -8,34 +8,38 @@ class UtilsCogs(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
+    async def load_ext(self, extension: list[str]):
+        for ext in extension:
+            try:
+                await self.bot.reload_extension(f"cogs.{cog}")
+            except Exception as e:
+                if isinstance(e, commands.ExtensionNotLoaded) or isinstance(e, commands.ExtensionNotFound):
+                    await self.bot.load_extension(f"cogs.{cog}")
+
     @commands.is_owner()
     @commands.command(name='reload')
     async def _reload(self, ctx: commands.Context, cog: str = None):
+        await ctx.message.delete()
         if cog is None:
-            await ctx.message.delete()
             return await ctx.send("Cog name needed!", delete_after=5)
         cogs = [file[:-3] for file in os.listdir("./cogs") if file.endswith(".py")]
-        if cog not in cogs:
-            await ctx.message.delete()
-            return await ctx.send(f"Cog `{cog}` does not exists!")
+        if cog not in cogs and cog != "all":
+            return await ctx.send(f"Cog `{cog}` does not exists!", delete_after=5)
+
+        if cog == "all":
+            await self.load_ext(cogs)
+        else:
+            await self.load_ext([cog])
         embed = discord.Embed(
-            title="Reloading Extension!",
-            description=f"**Cog: {cog}**",
+            title="Reloading...",
+            description=f"Extension: **{cog if cog != 'all' else ', '.join(cogs)}**",
             color=discord.Color.brand_red()
         )
-        await ctx.message.delete()
         msg = await ctx.send(embed=embed)
-        try:
-            await self.bot.reload_extension(f"cogs.{cog}")
-        except Exception as e:
-            if isinstance(e, commands.ExtensionNotLoaded) or isinstance(e, commands.ExtensionNotFound):
-                await self.bot.load_extension(f"cogs.{cog}")
         await msg.add_reaction("👍")
         await msg.delete(delay=5)
         return
 
 
-
 async def setup(bot):
     await bot.add_cog(UtilsCogs(bot))
-
