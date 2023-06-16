71957f08b62fe3d10bc57a6bca9fd65396f2d964
Salman Ali <sa1713791@gmail.com>
2023-06-16 23:32:11 +0545

    remove unnecessary env variables

--- utils/env.py
+++ utils/env.py
@@ -6,5 +6,3 @@ load_dotenv()
 
 YOUTUBE_API_KEY = os.environ['YOUTUBE_API_KEY']
 TOKEN = os.environ['TOKEN']
-MAX_PLAYLIST_ITEM = os.environ['MAX_PLAYLIST_ITEM']
-MAX_SEARCH_ITEM = os.environ['MAX_SEARCH_ITEM']
