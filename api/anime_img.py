f5c116549367f48484885f6fbe09d6395deceaae
Salman Ali <sa1713791@gmail.com>
2023-06-24 12:26:11 +0545

    change to endpoints list and function name

--- api/anime_img.py
+++ api/anime_img.py
@@ -3,9 +3,17 @@ from aiohttp import request
 
 waifu_base_url = "https://api.waifu.pics/"
 api_bi_base_url = "https://pic.re/"
+endpoints = [
+        "waifu", "neko", "shinobu", "megumin", "bully",
+        "cuddle", "cry", "hug", "awoo", "kiss", "lick",
+        "pat", "smug", "bonk", "yeet", "blush", "smile",
+        "wave", "highfive", "handhold", "nom", "bite",
+        "glomp", "slap", "kill", "kick", "happy", "wink",
+        "poke", "dance", "cringe"
+]
 
 
-async def get_waifu_api_image(endpoint):
+async def waifu_api_image(endpoint):
     async with request("GET", waifu_base_url + "sfw/" + endpoint, headers={}) as response:
         r = await response.json()
         return r.get("url")
@@ -17,19 +25,7 @@ async def get_api_bi_image():
         return r
 
 
-waifu_endpoints = {
-    "sfw": [
-        "waifu", "neko", "shinobu", "megumin", "bully",
-        "cuddle", "cry", "hug", "awoo", "kiss", "lick",
-        "pat", "smug", "bonk", "yeet", "blush", "smile",
-        "wave", "highfive", "handhold", "nom", "bite",
-        "glomp", "slap", "kill", "kick", "happy", "wink",
-        "poke", "dance", "cringe"
-    ]
-}
-
-
 async def get_anime_image(endpoint):
-    if endpoint in waifu_endpoints['sfw']:
-        return await get_waifu_api_image(endpoint)
+    if endpoint in endpoints:
+        return await waifu_api_image(endpoint)
     return None
